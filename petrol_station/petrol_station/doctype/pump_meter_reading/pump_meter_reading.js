5a3d246711c618daffe60ab37a1b7653eb5f8f6b
Mugula Abbey <mugieabbeym@gmail.com>
2026-02-06 18:52:39 +0300

    feat: enhance pump meter and meter reading management
    - Updated `Pump Meter Reading` doctype with new fields: employee, tank linkage, readings table, and total amount calculations.
    - Extended `Meter Reading` doctype to include opening/closing readings, testing liters, sales quantity, selling price, and total amount.
    - Adjusted `Pump Meter Reading` autoname format to use `{MM}` instead of `{M}`.
    - Added client script to filter nozzles based on selected pump.

--- petrol_station/petrol_station/doctype/pump_meter_reading/pump_meter_reading.js
+++ petrol_station/petrol_station/doctype/pump_meter_reading/pump_meter_reading.js
@@ -1,8 +1,14 @@
 // Copyright (c) 2026, Ignite Digital and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Pump Meter Reading", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Pump Meter Reading", {
+	refresh(frm) {
+            cur_frm.set_query("nozzle", "readings", function(doc, cdt, cdn) {
+            return {
+                filters: {
+                    "pump": doc.pump
+                }
+            }
+        })
+	},
+});
