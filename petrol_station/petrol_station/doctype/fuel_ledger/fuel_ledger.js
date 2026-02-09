13eae2d06b552a54fc542f9464896c62af629275
Mugula Abbey <mugieabbeym@gmail.com>
2026-02-09 09:53:07 +0300

    feat: Add management for shift, tank, and fuel meter readings
    - Added `Shift Opening Entry`, `Tank Dip Log`, `Fuel Ledger`, and `Shift Opening Meter Reading` doctypes.
    - Implemented support for fuel stock tracking, variance calculations, and linked shift operations.
    - Introduced client scripts, integration tests, and controllers for new doctypes.
    - Updated app version to `1.0.1`.

---
+++ petrol_station/petrol_station/doctype/fuel_ledger/fuel_ledger.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Ignite Digital and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Fuel Ledger", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ petrol_station/petrol_station/doctype/tank_dip_log/tank_dip_log.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Ignite Digital and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Tank Dip Log", {
+// 	refresh(frm) {
+
+// 	},
+// });
